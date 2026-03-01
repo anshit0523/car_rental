54d1fd154169478beba68111b0570c169e51ae80
anshit0523 <fernnadosaranajr@gmail.com>
2026-03-01 22:32:05 +0800

    remove form in landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -60,7 +60,7 @@
         }
 
         .hero {
-            padding: 180px 0 120px;
+            padding: 190px 0 135px;
             background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
                 url("{{ asset('storage/cars/toyota bg.png') }}");
             background-size: cover;
@@ -147,19 +147,7 @@
             <h1 class="display-4 fw-bold mb-3">DUMAGUETE EZE CAR RENTAL</h1>
             <p class="lead mb-4">Drive with Comfort & Confidence</p>
 
-            <div class="search-box mt-4">
-                <form class="row g-3">
-                    <div class="col-md-4">
-                        <input type="date" class="form-control">
-                    </div>
-                    <div class="col-md-4">
-                        <input type="date" class="form-control">
-                    </div>
-                    <div class="col-md-4">
-                        <button class="btn btn-orange w-100">Search</button>
-                    </div>
-                </form>
-            </div>
+            
         </div>
     </section>
 
