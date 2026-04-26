3a29b75afbc453cf2de02e2be94f66411a60bc6c
anshit0523 <fernnadosaranajr@gmail.com>
2026-04-26 23:20:27 +0800

    Force S3 filesystem at runtime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        config(['filesystems.default' => 's3']);
+
         View::composer('*', function ($view) {
             $pendingPaymentsCount = 0;
 
