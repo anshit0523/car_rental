3325d54e1e657dbe5293f75eb339a61f029209bc
anshit0523 <fernnadosaranajr@gmail.com>
2026-05-12 15:07:18 +0800

    tab title change

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title') | Dumaguete EZE Car Rental</title>
+    <title>@yield('title') | EZE Car Rental</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
